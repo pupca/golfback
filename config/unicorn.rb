1b8990c8c6dae9f229cef98f3d62e8cc73a2e0fe
Martin Popelak <pupca@pupca.cz>
2014-12-17 22:39:17 +0100

    a

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ if env == "production"
   working_directory "/home/ubuntu/apps/golfback/current"
 
   # feel free to point this anywhere accessible on the filesystem
-  user 'ubuntu', 'www-data'
+  user 'ubuntu', 'ubuntu'
   shared_path = "/home/ubuntu/apps/golfback/shared"
 
   # stderr_path "#{shared_path}/log/unicorn.stderr.log"
